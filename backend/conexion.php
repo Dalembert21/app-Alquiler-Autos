53ff0cf5b74892d17e0998c09db42fd88607ab34
KevinGanan <Kevinganan2901@gmail.com>
2024-10-08 23:11:17 -0500

    Cree la carpeta Backend, posterior a eso cree la conexion a la base de datos

---
+++ backend/conexion.php
@@ -0,0 +1,12 @@
+<?php
+define('server', 'localhost');
+define('db', 'uta_drive');
+define('user', 'root');
+define('password', '');
+try {
+    $conn = new PDO("mysql:host=" . server . ";dbname=" . db , user , password);
+    echo "Estas conectado";
+    return $conn;
+} catch (PDOException $e) {
+    echo "No estas conectado" . $e->getMessage();
+}
