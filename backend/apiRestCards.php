0bd8ad4af61ae6e08df93843ca82394ebb84fb91
KevinGanan <Kevinganan2901@gmail.com>
2024-11-17 13:53:11 -0500

    Agregue la funcion de registrar vehiculo al apiRestCards

--- backend/apiRestCards.php
+++ backend/apiRestCards.php
@@ -1,26 +1,36 @@
 <?php
 include_once "ConsultasCards.php";
-header('Content-Type: application/json'); 
+header('Content-Type: application/json');
 
 $action = $_GET['action'] ?? '';
 
-switch ($action) {
-    case 'obtenerVehiculos':
-        ModeloVehiculos::obtenerVehiculos();
-        break;
-    case 'obtenerTiposVehiculos':
-        ModeloVehiculos::obtenerTiposVehiculos();
-        break;
-    case 'obtenerAniosVehiculos':
-        ModeloVehiculos::obtenerAniosVehiculos();
-        break;
-    case 'obtenerTransmisiones':
-        ModeloVehiculos::obtenerTransmisiones();
-        break;
-    case 'obtenerCombustibles':
-        ModeloVehiculos::obtenerCombustibles();
-        break;
-    default:
-        echo json_encode(['mensaje' => 'Acción no válida']);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    switch ($action) {
+        case 'registrarVehiculo':
+            // Registrar un vehículo
+            ModeloVehiculos::registrarVehiculo();
+            break;
+        default:
+            echo json_encode(['mensaje' => 'Acción no válida']);
+    }
+} elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    switch ($action) {
+        case 'obtenerVehiculos':
+            ModeloVehiculos::obtenerVehiculos();
+            break;
+        case 'obtenerTiposVehiculos':
+            ModeloVehiculos::obtenerTiposVehiculos();
+            break;
+        case 'obtenerAniosVehiculos':
+            ModeloVehiculos::obtenerAniosVehiculos();
+            break;
+        case 'obtenerTransmisiones':
+            ModeloVehiculos::obtenerTransmisiones();
+            break;
+        case 'obtenerCombustibles':
+            ModeloVehiculos::obtenerCombustibles();
+            break;
+        default:
+            echo json_encode(['mensaje' => 'Acción no válida']);
+    }
 }
-?>
